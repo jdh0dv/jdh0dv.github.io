28a9d0022c305319a53d50c98baf8678b8a898a6
jdh0dv <djeong082@gmail.com>
2020-08-14 23:29:29 +0900

    header html 및 css 추가

--- js/default.js
+++ js/default.js
@@ -1,1 +1,9 @@
 'use strict';
+
+// 메뉴버튼 
+$(document).ready(function(){
+    $('.menuBtn').click(function(){
+        $(this).toggleClass('active');
+        $('.gnb').toggleClass('active');        
+    });
+})
