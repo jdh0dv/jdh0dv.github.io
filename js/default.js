efca4f02975bae7e7f99e63f3bde563f4df6c7d1
jdh0dv <djeong082@gmail.com>
2020-09-14 18:16:02 +0900

    header, home(section1) 마크업 및 css

--- js/default.js
+++ js/default.js
@@ -1,11 +1,57 @@
 'use strict';
 
-// project
-$(function(){
+
+$(document).ready(function(){
+    var section1 = '.home .visual'
+
+    //기본 애니메이션 효과
+    $('.header').css({
+        marginTop: 0,
+        opacity: 1
+    });
+
+    $(section1).css({
+        left: 0,
+        opacity: 1
+    });    
+    $(section1).find('p').css({
+        bottom: 0,
+        opacity: 1
+    });             
+
+    //scroll 애니메이션 효과
+
+
+    // 메뉴버튼 
+    $('.menu_btn_open').click(function(){
+        $('.gnb_area').addClass('active');  
+        $('.menu_btn_close').show();
+        $(this).hide();
+    });
+    $('.menu_btn_close').click(function(){
+        $('.gnb_area').removeClass('active');  
+        $('.menu_btn_open').show();
+        $(this).hide();        
+    });    
+
+    //scroll fix
+    $(window).scroll(function(){
+        var win_top = $(window).scrollTop()
+
+        if( win_top >= 1){
+            $('.header').addClass('fixed')
+            $('.home .goal').addClass('on')            
+        }else{
+            $('.header').removeClass('fixed')  
+            $('.home .goal').removeClass('on')               
+        }        
+    })
+
+    // project
     $('.project_item').mouseenter(function(){
         $(this).addClass('active')
     })
     $('.project_item').mouseleave(function(){
         $(this).removeClass('active')
-    })    
+    })     
 })
